530822077c8d358a37108fe99af8e7e5189350c6
Sandy Jones <sandynashv@gmail.com>
2016-06-03 17:57:41 -0500

    ctrler start

---
+++ app/controllers/WatchedCtrl.js
@@ -0,0 +1,22 @@
+<h1>SEARCH FOR MOVIES!</h1>
+<div class="row">
+    <div class="col-xs-6 col-xs-offset-3">
+        <div class="input-group search-bar-spacing">
+            <span class="input-group-btn">
+                <button class="btn btn-default"><i class="fa fa-search" aria-hidden="true"></i></button>
+            </span>
+            <input type="text" class="form-control" placeholder="Search for..." ng-model="searchText">
+        </div>
+    </div>
+</div>
+<h3>Watched Movies</h3>
+<div class="panel panel-default" >
+    <div class="panel-body">
+        <div class="col-xs-1"></div>
+        <div class="col-xs-5">Title</div>
+        <div class="col-xs-2">Director</div>
+        <div class="col-xs-1">Star</div> 
+        <div class="col-xs-1">Stars Graphic Here</div>
+        <div class="col-xs-1"></div>
+    </div>
+</div>
