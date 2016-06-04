f16e3cb92fa68fae12153506c4062a65ce095f6a
Andi Devois <devoisaurus@MacBook-Pro.local>
2016-06-04 10:39:53 -0500

    index setup

--- app/app.js
+++ app/app.js
@@ -1,39 +1,48 @@
-"use strict";
-var app = angular.module("MovieHistoryApp", [ng-route])
-//firebase URL to be setup and inserted below
-.constant(firebaseURL, "https");
+var app = angular.module("MovieHistoryApp", [])
 
-
-app.config(function($routeProvider){
-	$routeProvider.
-//unsure about this first "when section"
-		when('/', {
-			templateUrl: 'partial-html/frontPage.html'
-			controller: 'FrontPage',
-			resolve: {{isAuth}}
-		}).
-		when('/login', {
-			templateUrl: 'partial-html/login.html',
-			controller: 'app/controllers/LoginCtrl.js',
-			resolve: {{isAuth}}
-		}).
-		when('/moviesWatched/list', {
-			templateUrl: 'partial-html/list.html',
-			controller: 'app/controllers/WatchedCtrl.js'
-			resolve: {{isAuth}}
-		}).
-		when('/moviesToWatch/list', {
-			templateUrl: 'partial-html/toWatchList.html',
-			controller: 'app/controllers/ToWatchCtrl.js'
-			resolve: {{isAuth}}
-		}).
-		when('/logout', {
-			templateUrl: 'partial-html/login.html',
-			controller: "LoginCtrl.js"
-		}).
-		otherwise('/');
-
+app.controller("NavCtrl", function($scope) {
+	$scope.navItems = [{name: "Logout"}, {name:"Movies to watch"}, {name:"Watched movies"}]
 });
+
+
+
+
+// "use strict";
+// var app = angular.module("MovieHistoryApp", [ng-route])
+// //firebase URL to be setup and inserted below
+// .constant(firebaseURL, "https");
+
+
+// app.config(function($routeProvider){
+// 	$routeProvider.
+// //unsure about this first "when section"
+// 		when('/', {
+// 			templateUrl: 'partial-html/frontPage.html'
+// 			controller: 'FrontPage',
+// 			resolve: {{isAuth}}
+// 		}).
+// 		when('/login', {
+// 			templateUrl: 'partial-html/login.html',
+// 			controller: 'app/controllers/LoginCtrl.js',
+// 			resolve: {{isAuth}}
+// 		}).
+// 		when('/moviesWatched/list', {
+// 			templateUrl: 'partial-html/list.html',
+// 			controller: 'app/controllers/WatchedCtrl.js'
+// 			resolve: {{isAuth}}
+// 		}).
+// 		when('/moviesToWatch/list', {
+// 			templateUrl: 'partial-html/toWatchList.html',
+// 			controller: 'app/controllers/ToWatchCtrl.js'
+// 			resolve: {{isAuth}}
+// 		}).
+// 		when('/logout', {
+// 			templateUrl: 'partial-html/login.html',
+// 			controller: "LoginCtrl.js"
+// 		}).
+// 		otherwise('/');
+
+// });
 
 
 
